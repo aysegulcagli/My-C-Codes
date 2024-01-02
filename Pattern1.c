31665f0d3d8bb59e0d9be968cd354592e8d7bc2d
aysegulcagli <aysegulcagli@yandex.com>
2024-01-02 10:24:44 +0300

    Pattern 1

---
+++ Pattern1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+//Aysegul Cagli's homework
+
+int main( void )
+{
+	
+	int i;
+	int column;
+	
+	for(column=1; column <= 12; ++column){
+		for( i = 1; i <= 11; ++i){
+		if ( i == column ){
+			printf(" ");
+		}
+		else {
+			printf( "*");
+		}
+		}
+		printf(" \n");
+		}
+	}
+	
+	
+
+
+		
+	
+	
+		
+	
+
