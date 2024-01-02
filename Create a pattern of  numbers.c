9d8505981c7c2a30429afbae29a95eb2b39c900f
aysegulcagli <aysegulcagli@yandex.com>
2024-01-02 10:29:04 +0300

    Create a pattern of  numbers
    It takes an unsigned integer i as input from the user. It then uses nested loops to generate a pattern of numbers and prints the pattern to the console.

---
+++ Create a pattern of  numbers.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <math.h>
+//Aysegul Cagli's homework
+
+int main( void )
+{
+	unsigned int i;
+	int x;
+	int y;
+	
+	
+	printf(" Enter an integer:");
+	scanf("%u", &i);
+	
+    for(x=1;x<=i;x++)
+    {
+  for(y=x;y<=i;++y )
+   {
+   printf("%d", y);
+   }
+   for(y=1;y<=x-1;++y){
+   	printf("%d", y);
+   }
+  printf("\n");
+ }
+    
+    
+		
+}
+    	
+		
+		
+		
+		
+	
+	
+  
+  
+  
+ 
+
+		
+    	
+   
+   
+   
+   
+   	
+   
+  
+ 
+	
+	
+	
+	
+	
+
+	
+	
+		
+			
+		
+		
+	
+		
+	
+	
+	
+	
+	
+	
+	
+
+
